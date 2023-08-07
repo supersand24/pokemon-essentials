a09b45e38c14c2779ebc0eee8560415468e29965
supersand24 <supersand24@gmail.com>
2023-08-07 15:08:46 -0400

    Imported old assets

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -6,7 +6,7 @@
 
 module Settings
   # The version of your game. It has to adhere to the MAJOR.MINOR.PATCH format.
-  GAME_VERSION = "1.0.0"
+  GAME_VERSION = "0.3.0"
 
   # The generation that the battle system follows. Used throughout the battle
   # scripts, and also by some other settings which are used in and out of battle
@@ -35,11 +35,11 @@ module Settings
   # The level of newly hatched Pokémon.
   EGG_LEVEL            = 1
   # The odds of a newly generated Pokémon being shiny (out of 65536).
-  SHINY_POKEMON_CHANCE = (MECHANICS_GENERATION >= 6) ? 16 : 8
+  SHINY_POKEMON_CHANCE = 1024
   # Whether super shininess is enabled (uses a different shiny animation).
-  SUPER_SHINY          = (MECHANICS_GENERATION >= 8)
+  SUPER_SHINY          = 1
   # The odds of a wild Pokémon/bred egg having Pokérus (out of 65536).
-  POKERUS_CHANCE       = 3
+  POKERUS_CHANCE       = 1024
   # Whether IVs and EVs are treated as 0 when calculating a Pokémon's stats.
   # IVs and EVs still exist, and are used by Hidden Power and some cosmetic
   # things as normal.
@@ -52,16 +52,16 @@ module Settings
   # Whether the reflections of the player/events will ripple horizontally.
   ANIMATE_REFLECTIONS                        = true
   # Whether poisoned Pokémon will lose HP while walking around in the field.
-  POISON_IN_FIELD                            = (MECHANICS_GENERATION <= 4)
+  POISON_IN_FIELD                            = true
   # Whether poisoned Pokémon will faint while walking around in the field
   # (true), or survive the poisoning with 1 HP (false).
-  POISON_FAINT_IN_FIELD                      = (MECHANICS_GENERATION <= 3)
+  POISON_FAINT_IN_FIELD                      = false
   # Whether planted berries grow according to Gen 4 mechanics (true) or Gen 3
   # mechanics (false).
   NEW_BERRY_PLANTS                           = (MECHANICS_GENERATION >= 4)
   # Whether fishing automatically hooks the Pokémon (true), or whether there is
   # a reaction test first (false).
-  FISHING_AUTO_HOOK                          = false
+  FISHING_AUTO_HOOK                          = true
   # The ID of the common event that runs when the player starts fishing (runs
   # instead of showing the casting animation).
   FISHING_BEGIN_COMMON_EVENT                 = -1
@@ -71,23 +71,23 @@ module Settings
   # Whether Pokémon in the Day Care gain Exp for each step the player takes.
   # This is true for the Day Care and false for the Pokémon Nursery, both of
   # which use the same code in Essentials.
-  DAY_CARE_POKEMON_GAIN_EXP_FROM_WALKING     = (MECHANICS_GENERATION <= 6)
+  DAY_CARE_POKEMON_GAIN_EXP_FROM_WALKING     = true
   # Whether two Pokémon in the Day Care can learn egg moves from each other if
   # they are the same species.
-  DAY_CARE_POKEMON_CAN_SHARE_EGG_MOVES       = (MECHANICS_GENERATION >= 8)
+  DAY_CARE_POKEMON_CAN_SHARE_EGG_MOVES       = true
   # Whether a bred baby Pokémon can inherit any TM/TR/HM moves from its father.
   # It can never inherit TM/TR/HM moves from its mother.
-  BREEDING_CAN_INHERIT_MACHINE_MOVES         = (MECHANICS_GENERATION <= 5)
+  BREEDING_CAN_INHERIT_MACHINE_MOVES         = true
   # Whether a bred baby Pokémon can inherit egg moves from its mother. It can
   # always inherit egg moves from its father.
-  BREEDING_CAN_INHERIT_EGG_MOVES_FROM_MOTHER = (MECHANICS_GENERATION >= 6)
+  BREEDING_CAN_INHERIT_EGG_MOVES_FROM_MOTHER = true
   # Whether the Pokédex entry of a newly owned species will be shown after it
   # hatches from an egg, after it evolves and after obtaining it from a trade,
   # in addition to after catching it in battle.
-  SHOW_NEW_SPECIES_POKEDEX_ENTRY_MORE_OFTEN  = (MECHANICS_GENERATION >= 7)
+  SHOW_NEW_SPECIES_POKEDEX_ENTRY_MORE_OFTEN  = true
   # Whether you get 1 Premier Ball for every 10 of any kind of Poké Ball bought
   # at once (true), or 1 Premier Ball for buying 10+ Poké Balls (false).
-  MORE_BONUS_PREMIER_BALLS                   = (MECHANICS_GENERATION >= 8)
+  MORE_BONUS_PREMIER_BALLS                   = true
   # The number of steps allowed before a Safari Zone game is over (0=infinite).
   SAFARI_STEPS                               = 600
   # The number of seconds a Bug-Catching Contest lasts for (0=infinite).
@@ -98,46 +98,46 @@ module Settings
   # If a move taught by a TM/HM/TR replaces another move, this setting is
   # whether the machine's move retains the replaced move's PP (true), or whether
   # the machine's move has full PP (false).
-  TAUGHT_MACHINES_KEEP_OLD_PP          = (MECHANICS_GENERATION == 5)
+  TAUGHT_MACHINES_KEEP_OLD_PP          = true
   # Whether the Move Relearner can also teach egg moves that the Pokémon knew
   # when it hatched and moves that the Pokémon was once taught by a TR. Moves
   # from the Pokémon's level-up moveset of the same or a lower level than the
   # Pokémon can always be relearned.
-  MOVE_RELEARNER_CAN_TEACH_MORE_MOVES  = (MECHANICS_GENERATION >= 6)
+  MOVE_RELEARNER_CAN_TEACH_MORE_MOVES  = true
   # Whether various HP-healing items heal the amounts they do in Gen 7+ (true)
   # or in earlier Generations (false).
-  REBALANCED_HEALING_ITEM_AMOUNTS      = (MECHANICS_GENERATION >= 7)
+  REBALANCED_HEALING_ITEM_AMOUNTS      = true
   # Whether Rage Candy Bar acts as a Full Heal (true) or a Potion (false).
-  RAGE_CANDY_BAR_CURES_STATUS_PROBLEMS = (MECHANICS_GENERATION >= 7)
+  RAGE_CANDY_BAR_CURES_STATUS_PROBLEMS = true
   # Whether vitamins can add EVs no matter how many that stat already has in it
   # (true), or whether they can't make that stat's EVs greater than 100 (false).
-  NO_VITAMIN_EV_CAP                    = (MECHANICS_GENERATION >= 8)
+  NO_VITAMIN_EV_CAP                    = true
   # Whether Rare Candy can be used on a Pokémon that is already at its maximum
   # level if it is able to evolve by level-up (if so, triggers that evolution).
-  RARE_CANDY_USABLE_AT_MAX_LEVEL       = (MECHANICS_GENERATION >= 8)
+  RARE_CANDY_USABLE_AT_MAX_LEVEL       = true
   # Whether the player can choose how many of an item to use at once on a
   # Pokémon. This applies to Exp-changing items (Rare Candy, Exp Candies) and
   # EV-changing items (vitamins, feathers, EV-lowering berries).
-  USE_MULTIPLE_STAT_ITEMS_AT_ONCE      = (MECHANICS_GENERATION >= 8)
+  USE_MULTIPLE_STAT_ITEMS_AT_ONCE      = true
 
   #=============================================================================
 
   # Whether Repel uses the level of the first Pokémon in the party regardless of
   # its HP (true), or it uses the level of the first unfainted Pokémon (false).
-  REPEL_COUNTS_FAINTED_POKEMON             = (MECHANICS_GENERATION >= 6)
+  REPEL_COUNTS_FAINTED_POKEMON             = true
   # Whether more abilities affect whether wild Pokémon appear, which Pokémon
   # they are, etc.
-  MORE_ABILITIES_AFFECT_WILD_ENCOUNTERS    = (MECHANICS_GENERATION >= 8)
+  MORE_ABILITIES_AFFECT_WILD_ENCOUNTERS    = true
   # Whether the Black/White Flutes will raise/lower the levels of wild Pokémon
   # respectively (true), or will lower/raise the wild encounter rate
   # respectively (false).
-  FLUTES_CHANGE_WILD_ENCOUNTER_LEVELS      = (MECHANICS_GENERATION >= 6)
+  FLUTES_CHANGE_WILD_ENCOUNTER_LEVELS      = true
   # Whether shiny wild Pokémon are more likely to appear if the player has
   # previously defeated/caught lots of other Pokémon of the same species.
-  HIGHER_SHINY_CHANCES_WITH_NUMBER_BATTLED = (MECHANICS_GENERATION >= 8)
+  HIGHER_SHINY_CHANCES_WITH_NUMBER_BATTLED = true
   # Whether overworld weather can set the default terrain effect in battle.
   # Storm weather sets Electric Terrain, and fog weather sets Misty Terrain.
-  OVERWORLD_WEATHER_SETS_BATTLE_TERRAIN    = (MECHANICS_GENERATION >= 8)
+  OVERWORLD_WEATHER_SETS_BATTLE_TERRAIN    = true
   # The default setting for Phone.rematches_enabled, which determines whether
   # trainers registered in the Phone can become ready for a rematch. If false,
   # Phone.rematches_enabled = true will enable rematches at any point you want.
@@ -153,11 +153,7 @@ module Settings
   # A set of arrays each containing a trainer type followed by a Game Variable
   # number. If the Variable isn't set to 0, then all trainers with the
   # associated trainer type will be named as whatever is in that Variable.
-  RIVAL_NAMES = [
-    [:RIVAL1,   12],
-    [:RIVAL2,   12],
-    [:CHAMPION, 12]
-  ]
+  RIVAL_NAMES = []
 
   #=============================================================================
 
@@ -171,12 +167,12 @@ module Settings
   # second badge, etc.
   #   e.g. To require the second badge, put false and 1.
   #        To require at least 2 badges, put true and 2.
-  BADGE_FOR_CUT       = 1
-  BADGE_FOR_FLASH     = 2
-  BADGE_FOR_ROCKSMASH = 3
-  BADGE_FOR_SURF      = 4
+  BADGE_FOR_CUT       = 4
+  BADGE_FOR_FLASH     = 6
+  BADGE_FOR_ROCKSMASH = 2
+  BADGE_FOR_SURF      = 1
   BADGE_FOR_FLY       = 5
-  BADGE_FOR_STRENGTH  = 6
+  BADGE_FOR_STRENGTH  = 3
   BADGE_FOR_DIVE      = 7
   BADGE_FOR_WATERFALL = 8
 
@@ -200,22 +196,22 @@ module Settings
   # Whether each pocket in turn auto-sorts itself by item ID number.
   BAG_POCKET_AUTO_SORT = [false, false, false, true, true, false, false, false]
   # The maximum number of items each slot in the Bag can hold.
-  BAG_MAX_PER_SLOT     = 999
+  BAG_MAX_PER_SLOT     = 99
 
   #=============================================================================
 
   # The number of boxes in Pokémon storage.
-  NUM_STORAGE_BOXES   = 40
+  NUM_STORAGE_BOXES   = 2
   # Whether putting a Pokémon into Pokémon storage will heal it. IF false, they
   # are healed by the Recover All: Entire Party event command (at Poké Centers).
-  HEAL_STORED_POKEMON = (MECHANICS_GENERATION <= 7)
+  HEAL_STORED_POKEMON = false
 
   #=============================================================================
 
   # Whether the Pokédex list shown is the one for the player's current region
   # (true), or whether a menu pops up for the player to manually choose which
   # Dex list to view if more than one is available (false).
-  USE_CURRENT_REGION_DEX = false
+  USE_CURRENT_REGION_DEX = true
   # The names of the Pokédex lists, in the order they are defined in the PBS
   # file "regional_dexes.txt". The last name is for the National Dex and is
   # added onto the end of this array (remember that you don't need to use it).
@@ -229,8 +225,7 @@ module Settings
   # that Dex list, no matter which region the player is currently in.
   def self.pokedex_names
     return [
-      [_INTL("Kanto Pokédex"), 0],
-      [_INTL("Johto Pokédex"), 1],
+      [_INTL("Hoenn Pokédex"), 0],
       _INTL("National Pokédex")
     ]
   end
@@ -254,10 +249,7 @@ module Settings
   #   * Y coordinate of the graphic on the map, in squares.
   #   * Name of the graphic, found in the Graphics/UI/Town Map folder.
   #   * The graphic will always (true) or never (false) be shown on a wall map.
-  REGION_MAP_EXTRAS = [
-    [0, 51, 16, 15, "hidden_Berth", false],
-    [0, 52, 20, 14, "hidden_Faraday", false]
-  ]
+  REGION_MAP_EXTRAS = []
 
   # Whether the player can use Fly while looking at the Town Map. This is only
   # allowed if the player can use Fly normally.
@@ -277,18 +269,7 @@ module Settings
 
   # A list of maps used by roaming Pokémon. Each map has an array of other maps
   # it can lead to.
-  ROAMING_AREAS = {
-    5  => [   21, 28, 31, 39, 41, 44, 47, 66, 69],
-    21 => [5,     28, 31, 39, 41, 44, 47, 66, 69],
-    28 => [5, 21,     31, 39, 41, 44, 47, 66, 69],
-    31 => [5, 21, 28,     39, 41, 44, 47, 66, 69],
-    39 => [5, 21, 28, 31,     41, 44, 47, 66, 69],
-    41 => [5, 21, 28, 31, 39,     44, 47, 66, 69],
-    44 => [5, 21, 28, 31, 39, 41,     47, 66, 69],
-    47 => [5, 21, 28, 31, 39, 41, 44,     66, 69],
-    66 => [5, 21, 28, 31, 39, 41, 44, 47,     69],
-    69 => [5, 21, 28, 31, 39, 41, 44, 47, 66    ]
-  }
+  ROAMING_AREAS = {}
   # A set of arrays, each containing the details of a roaming Pokémon. The
   # information within each array is as follows:
   #   * Species.
@@ -298,17 +279,7 @@ module Settings
   #     4=surfing/fishing). See the bottom of PField_RoamingPokemon for lists.
   #   * Name of BGM to play for that encounter (optional).
   #   * Roaming areas specifically for this Pokémon (optional).
-  ROAMING_SPECIES = [
-    [:LATIAS, 30, 53, 0, "Battle roaming"],
-    [:LATIOS, 30, 53, 0, "Battle roaming"],
-    [:KYOGRE, 40, 54, 2, nil, {
-      2  => [   21, 31    ],
-      21 => [2,     31, 69],
-      31 => [2, 21,     69],
-      69 => [   21, 31    ]
-    }],
-    [:ENTEI, 40, 55, 1]
-  ]
+  ROAMING_SPECIES = []
 
   #=============================================================================
 
@@ -439,17 +410,18 @@ module Settings
   # automatically.
   def self.game_credits
     return [
-      _INTL("My Game by:"),
-      "Maruno",
+      _INTL("Pokemon Infinite by:"),
+      "supersand24",
       "",
-      _INTL("Also involved were:"),
-      "A. Lee Uss<s>Anne O'Nymus",
-      "Ecksam Pell<s>Jane Doe",
-      "Joe Dan<s>Nick Nayme",
-      "Sue Donnim<s>",
+      _INTL("Special Thanks to:"),
+      "Pokemon Essentials<s>Maruno",
+      "Automatic Level Scaling<s>Benitex",
       "",
-      _INTL("Special thanks to:"),
-      "Pizza"
+      _INTL("Also Thanks to:"),
+      "Thieving Items into Bag<s>Appletun",
+      "",
+      _INTL("Alpha/Beta Testers:"),
+      "sqKweglord",
     ]
   end
 end
--- Data/Scripts/002_BattleSettings.rb
+++ Data/Scripts/002_BattleSettings.rb
@@ -1,8 +1,8 @@
 module Settings
   # Whether turn order is recalculated after a Pokémon Mega Evolves.
-  RECALCULATE_TURN_ORDER_AFTER_MEGA_EVOLUTION = (MECHANICS_GENERATION >= 7)
+  RECALCULATE_TURN_ORDER_AFTER_MEGA_EVOLUTION = true
   # Whether turn order is recalculated after a Pokémon's Speed stat changes.
-  RECALCULATE_TURN_ORDER_AFTER_SPEED_CHANGES  = (MECHANICS_GENERATION >= 8)
+  RECALCULATE_TURN_ORDER_AFTER_SPEED_CHANGES  = true
   # Whether any Pokémon (originally owned by the player or foreign) can disobey
   # the player's commands if the Pokémon is too high a level compared to the
   # number of Gym Badges the player has.
@@ -12,11 +12,11 @@ module Settings
   FOREIGN_HIGH_LEVEL_POKEMON_CAN_DISOBEY      = true
   # Whether a move's physical/special category depends on the move itself as in
   # newer Gens (true), or on its type as in older Gens (false).
-  MOVE_CATEGORY_PER_MOVE                      = (MECHANICS_GENERATION >= 4)
+  MOVE_CATEGORY_PER_MOVE                      = true
   # Whether critical hits do 1.5x damage and have 4 stages (true), or they do 2x
   # damage and have 5 stages as in Gen 5 (false). Also determines whether
   # critical hit rate can be copied by Transform/Psych Up.
-  NEW_CRITICAL_HIT_RATE_MECHANICS             = (MECHANICS_GENERATION >= 6)
+  NEW_CRITICAL_HIT_RATE_MECHANICS             = true
 
   #=============================================================================
 
@@ -25,40 +25,40 @@ module Settings
   #   * Ghost-type immunity to being trapped
   #   * Grass-type immunity to powder moves and Effect Spore
   #   * Poison-type Pokémon can't miss when using Toxic
-  MORE_TYPE_EFFECTS                   = (MECHANICS_GENERATION >= 6)
+  MORE_TYPE_EFFECTS                   = true
   # Whether weather caused by an ability lasts 5 rounds (true) or forever (false).
-  FIXED_DURATION_WEATHER_FROM_ABILITY = (MECHANICS_GENERATION >= 6)
+  FIXED_DURATION_WEATHER_FROM_ABILITY = true
   # Whether X items (X Attack, etc.) raise their stat by 2 stages (true) or 1
   # (false).
-  X_STAT_ITEMS_RAISE_BY_TWO_STAGES    = (MECHANICS_GENERATION >= 7)
+  X_STAT_ITEMS_RAISE_BY_TWO_STAGES    = true
   # Whether some Poké Balls have catch rate multipliers from Gen 7 (true) or
   # from earlier generations (false).
-  NEW_POKE_BALL_CATCH_RATES           = (MECHANICS_GENERATION >= 7)
+  NEW_POKE_BALL_CATCH_RATES           = true
   # Whether Soul Dew powers up Psychic and Dragon-type moves by 20% (true) or
   # raises the holder's Special Attack and Special Defense by 50% (false).
-  SOUL_DEW_POWERS_UP_TYPES            = (MECHANICS_GENERATION >= 7)
+  SOUL_DEW_POWERS_UP_TYPES            = true
 
   #=============================================================================
 
   # Whether Pokémon with high happiness will gain more Exp from battles, have a
   # chance of avoiding/curing negative effects by themselves, resisting
   # fainting, etc.
-  AFFECTION_EFFECTS        = false
+  AFFECTION_EFFECTS        = true
   # Whether a Pokémon's happiness is limited to 179, and can only be increased
   # further with friendship-raising berries. Related to AFFECTION_EFFECTS by
   # default because affection effects only start applying above a happiness of
   # 179. Also lowers the happiness evolution threshold to 160.
-  APPLY_HAPPINESS_SOFT_CAP = AFFECTION_EFFECTS
+  APPLY_HAPPINESS_SOFT_CAP = true
 
   #=============================================================================
 
   # The minimum number of badges required to boost each stat of a player's
   # Pokémon by 1.1x, in battle only.
-  NUM_BADGES_BOOST_ATTACK  = (MECHANICS_GENERATION >= 4) ? 999 : 1
-  NUM_BADGES_BOOST_DEFENSE = (MECHANICS_GENERATION >= 4) ? 999 : 5
-  NUM_BADGES_BOOST_SPATK   = (MECHANICS_GENERATION >= 4) ? 999 : 7
-  NUM_BADGES_BOOST_SPDEF   = (MECHANICS_GENERATION >= 4) ? 999 : 7
-  NUM_BADGES_BOOST_SPEED   = (MECHANICS_GENERATION >= 4) ? 999 : 3
+  NUM_BADGES_BOOST_ATTACK  = 8
+  NUM_BADGES_BOOST_DEFENSE = 8
+  NUM_BADGES_BOOST_SPATK   = 8
+  NUM_BADGES_BOOST_SPDEF   = 8
+  NUM_BADGES_BOOST_SPEED   = 8
 
   #=============================================================================
 
@@ -70,29 +70,29 @@ module Settings
 
   # Whether the Exp gained from beating a Pokémon should be scaled depending on
   # the gainer's level.
-  SCALED_EXP_FORMULA                   = (MECHANICS_GENERATION == 5 || MECHANICS_GENERATION >= 7)
+  SCALED_EXP_FORMULA                   = true
   # Whether the Exp gained from beating a Pokémon should be divided equally
   # between each participant (true), or whether each participant should gain
   # that much Exp (false). This also applies to Exp gained via the Exp Share
   # (held item version) being distributed to all Exp Share holders.
-  SPLIT_EXP_BETWEEN_GAINERS            = (MECHANICS_GENERATION <= 5)
+  SPLIT_EXP_BETWEEN_GAINERS            = true
   # Whether the Exp gained from beating a Pokémon is multiplied by 1.5 if that
   # Pokémon is owned by another trainer.
-  MORE_EXP_FROM_TRAINER_POKEMON        = (MECHANICS_GENERATION <= 6)
+  MORE_EXP_FROM_TRAINER_POKEMON        = true
   # Whether a Pokémon holding a Power item gains 8 (true) or 4 (false) EVs in
   # the relevant stat.
-  MORE_EVS_FROM_POWER_ITEMS            = (MECHANICS_GENERATION >= 7)
+  MORE_EVS_FROM_POWER_ITEMS            = true
   # Whether the critical capture mechanic applies. Note that its calculation is
   # based on a total of 600+ species (i.e. that many species need to be caught
   # to provide the greatest critical capture chance of 2.5x), and there may be
   # fewer species in your game.
-  ENABLE_CRITICAL_CAPTURES             = (MECHANICS_GENERATION >= 5)
+  ENABLE_CRITICAL_CAPTURES             = false
   # Whether Pokémon gain Exp for capturing a Pokémon.
-  GAIN_EXP_FOR_CAPTURE                 = (MECHANICS_GENERATION >= 6)
+  GAIN_EXP_FOR_CAPTURE                 = true
   # Whether the player is asked what to do with a newly caught Pokémon if their
   # party is full. If true, the player can toggle whether they are asked this in
   # the Options screen.
-  NEW_CAPTURE_CAN_REPLACE_PARTY_MEMBER = (MECHANICS_GENERATION >= 7)
+  NEW_CAPTURE_CAN_REPLACE_PARTY_MEMBER = true
 
   #=============================================================================
 
@@ -101,7 +101,7 @@ module Settings
   NO_MONEY_LOSS                       = 33
   # Whether party Pokémon check whether they can evolve after all battles
   # regardless of the outcome (true), or only after battles the player won (false).
-  CHECK_EVOLUTION_AFTER_ALL_BATTLES   = (MECHANICS_GENERATION >= 6)
+  CHECK_EVOLUTION_AFTER_ALL_BATTLES   = true
   # Whether fainted Pokémon can try to evolve after a battle.
   CHECK_EVOLUTION_FOR_FAINTED_POKEMON = true
 
